2c8cb36079865674de918f5dec5106b55caaf71b
jxyyjm <jxyyjm@gmail.com>
2020-06-13 16:31:26 +0800

    Create EstimatorSaveModel.py
    a customed tf.estimator.export_save_model using "serving_input_receiver_fn".

---
+++ EstimatorSaveModel.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+'''
+  here save the tf.estimator.export_saved_model for TF-Serving to receive proto-buffer data 
+  notice: TF-Version 2.0.0
+'''
+import os
+import time
+import numpy as np
+import tensorflow as tf
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+def build_column():
+  feat1_column = tf.feature_column.numeric_column('feat1')
+  feat2_column = tf.feature_column.numeric_column('feat2')
+  feat3_column = tf.feature_column.numeric_column('feat3')
+  feat4_column = tf.feature_column.numeric_column('feat4')
+  feature_columns = [feat1_column, feat2_column, feat3_column, feat4_column]
+  return feature_columns
+
+#def input_fn(x, y):
+#  dataset = tf.data.Dataset.from_tensor_slices((x, y))
+#  dataset = dataset.shuffle(buffer_size=100).batch(16)
+#  return dataset
+
+def input_fn(file_name):
+  def decode_line(line):
+    columns_value = tf.io.decode_csv(line, record_defaults=[0.0, 0.0, 0.0, 0.0, 0], field_delim='\t')
+    return dict(zip(['feat1', 'feat2', 'feat3', 'feat4'], columns_value[0:4])), columns_value[-1]
+    #return dict(zip(['feat1', 'feat2', 'feat3', 'feat4', 'label'], columns_value))
+  dataset = tf.data.TextLineDataset(file_name)
+  dataset = dataset.map(decode_line)
+  dataset = dataset.batch(16)
+  return dataset
+  #x, y = dataset.make_one_shot_iterator().get_next()
+  #return x, y
+
+def _serving_input_receiver_fn():
+  input_feature = { \
+    'feat1': tf.io.FixedLenFeature([1], tf.float32), \
+    'feat2': tf.io.FixedLenFeature([1], tf.float32), \
+    'feat3': tf.io.FixedLenFeature([1], tf.float32), \
+    'feat4': tf.io.FixedLenFeature([1], tf.float32), \
+  }
+  tf.compat.v1.disable_eager_execution()
+  serialized_tf_example = tf.compat.v1.placeholder( \
+                  dtype = tf.string, \
+                  shape = (None), \
+                  name  = 'input_example_tensor')
+  features = tf.io.parse_example(serialized_tf_example, input_feature)
+  receiver_tensors = {'examples': serialized_tf_example}
+  return tf.estimator.export.ServingInputReceiver(features, receiver_tensors)
+  #return tf.estimator.export.TensorServingInputReceiver(features, receiver_tensors)
+
+def main():
+
+  estimator = tf.estimator.DNNEstimator( \
+          head = tf.estimator.BinaryClassHead(), \
+          feature_columns = build_column(), \
+          hidden_units = [16, 20], \
+          model_dir = '../model/', \
+          )
+  for i in range(3):
+    estimator.train(input_fn=lambda: input_fn('./random.data'))
+    accu = estimator.evaluate(input_fn=lambda: input_fn('./random.data.test'), steps=100)
+    print ('i=', i, 'accu', accu)
+
+  tf.compat.v1.disable_eager_execution()
+  estimator.export_saved_model( \
+    export_dir_base = '../model/lastest/', \
+    serving_input_receiver_fn = _serving_input_receiver_fn
+  )
+  
+
+  params = {}
+  params['columns'] = build_column()
+  #data_train = input_fn('./random.data', 2, True, 16)
+
+if __name__ =='__main__':
+  gpus = tf.config.experimental.list_physical_devices(device_type='GPU')
+  for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+  #tf.config.experimental.set_visible_devices(gpus[1], 'GPU')
+  main()
